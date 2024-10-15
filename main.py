3af77cc2759f92167eb3a13fbf053b8c4d166f07
mix <dbkickva@outlook.com>
2024-10-15 16:00:40 -0400

    Fix logic error with multiple URLs
    This also removes the 'sys' import.

--- main.py
+++ main.py
@@ -2,17 +2,22 @@
 #Requests and html2text must be installed
 import requests
 import html2text
-import sys
+import json
 
 #the main function, gets contents of page
 def scrape(url, keyword):
-    #get the data form the url
+    #get the data form the url, variable below is counter, loop through list
     urlc = 0
     for string in url:
         response = requests.get(url[urlc])
         #make sure it's a successfull request
         if response.status_code != 200:
-            print(response)
+            if response.status_code == 404:
+                print("404 not found")
+            elif response.status_code == 403:
+                print("403 forbidden")
+            else:
+                print(response.status_code)
             print("request FAILED! The server/site directory doesn't exist or doesn't want you etc.")
             return "failed, connection error"
         else:
@@ -36,7 +41,7 @@ def cleantext(content):
     return text
 
 
-#ask for URL and keyword (this is stupid)
+#ask for URL and keyword
 url = input('Enter your URL(s)\n')
 url = url.split()
 keyw = input('Enter a keyword to search for (leave blank for no keyword)\n')
@@ -48,23 +53,23 @@ for string in url:
         url[urld] = "http://" + url[urld]
     urld += 1
 
-
-html = scrape(url, keyw)
-cleaned = cleantext(html)
-
-#ask if they want terminal output
-if "failed" in html:
-    print(html)
-else:
-    show = input("Print HTML source? y/n\n").lower()
-    show2 = input("Print cleaned text? y/n\n").lower()
-    if show == "y":
+# big ugly loop
+for string in url:
+    html = scrape(url, keyw)
+    cleaned = cleantext(html)
+    if "failed" in html:
         print(html)
-    if show2 == "y":
-        print(cleaned)
-
-# export data to file for data extraction
-print('Exported to data.txt')
-sys.stdout = open('data.txt', 'wt')
-print(html)
-print(cleaned)
+    else:
+        #ask if they want terminal output
+        show = input("Print HTML source? y/n\n").lower()
+        show2 = input("Print cleaned text? y/n\n").lower()
+        if show == "y":
+            print(html)
+        if show2 == "y":
+            print(cleaned)
+    # export data to file for data extraction
+    with open('data.txt', 'a') as f:
+        f.write(str(html))
+        f.write(cleaned)
+        f.close()
+    print('Exported to data.txt')
